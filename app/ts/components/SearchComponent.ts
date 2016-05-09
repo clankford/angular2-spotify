0fdd74d0714d54b3df5f55d58ef0337d5993bf0e
Chris Lankford <christopherlankford@gmail.com>
2016-05-09 07:14:49 -0700

    Implemented the SearchComponent controller.

--- app/ts/components/SearchComponent.ts
+++ app/ts/components/SearchComponent.ts
@@ -61,3 +61,38 @@ import { SpotifyService } from '../services/SpotifyService';
     </div>
     `
 })
+export class SearchComponent implements OnInit {
+    query: string;
+    results: Object;
+    
+    constructor(public spotify: SpotifyService, public router: Router,
+                public routeParams: RouteParams) {                      
+    }
+    
+    ngOnInit(): void {
+        this.search();
+    }
+    
+    submit(query: string): void {
+        this.router.navigate(['/Search', {query: query}]);
+        this.search();
+    }
+    
+    search(): void {
+        this.query = this.routeParams.get('query');
+        if (!this.query) {
+            return;
+        }
+        
+        this.spotify
+            .searchByTrack(this.query)
+            .subscribe((res:any) => this.renderResults(res));
+    }
+    
+    renderResults(res: any): void {
+        this.results = null;
+        if (res && res.tracks && res.tracks.items) {
+            this.results = res.tracks.items;
+        }
+    }
+}
