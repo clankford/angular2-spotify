267076506fc7ada586e16aadf15ee6136e3edc8c
Chris Lankford <christopherlankford@gmail.com>
2016-05-10 11:24:20 -0700

    Added Artist component

---
+++ app/ts/components/ArtistComponent.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from 'angular2/core';
+import { CORE_DIRECTIVES } from 'angular2/common';
+import { RouteParams } from 'angular2/router';
+import { LocationStrategy } from 'angular2/platform/common';
+import { SpotifyService } from '../services/SpotifyService';
+
+@Component({
+    selector: 'artist',
+    directives: [CORE_DIRECTIVES],
+    template: `
+    <div *ngIf="artist">
+        <h1>{{ artist.name }}</h1>
+        
+        <p>
+            <img src="{{ artist.images[0].url }}>
+        </p>
+        
+        <p><a href (click)="back()">Back</a></p>
+    </div>
+    `
+})
+export class ArtistComponent implements OnInit {
+    id: string;
+    artist: Object;
+    
+    constructor(public routeParams: RouteParams, public spotify: SpotifyService,
+              public locationStrategy: LocationStrategy) {
+    this.id = routeParams.get('id');
+  }
+  
+  ngOnInit(): void {
+    this.spotify
+      .getArtist(this.id)
+      .subscribe((res: any) => this.renderArtist(res));
+  }
+
+  back(): void {
+    this.locationStrategy.back();
+  }
+
+  renderArtist(res: any): void {
+    this.artist = res;
+  }
+}
